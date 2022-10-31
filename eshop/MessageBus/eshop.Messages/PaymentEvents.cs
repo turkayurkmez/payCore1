5c20aaddd37fb10f34f35b6cbab34afa70b6d697
Türkay Ürkmez <turkay@turkayurkmez.com>
2022-10-31 12:14:03 +0300

    All event flow creating

---
+++ eshop/MessageBus/eshop.Messages/PaymentEvents.cs
@@ -0,0 +1,15 @@
+﻿namespace eshop.Messages
+{
+	public class PaymentCompleted
+	{
+		public int OrderId { get; set; }
+
+	}
+
+	public class PaymentFailed
+	{
+		public int OrderId { get; set; }
+		public string Message { get; set; }
+	}
+
+}
---
+++ eshop/Services/Payment/Payment.API/Consumers/StockReservedConsumer.cs
@@ -0,0 +1,32 @@
+﻿using eshop.Messages;
+using MassTransit;
+
+namespace Payment.API.Consumers
+{
+    public class StockReservedConsumer : IConsumer<StockReserved>
+    {
+        private readonly IPublishEndpoint publishEndpoint;
+
+        public StockReservedConsumer(IPublishEndpoint publishEndpoint)
+        {
+            this.publishEndpoint = publishEndpoint;
+        }
+
+        public async Task Consume(ConsumeContext<StockReserved> context)
+        {
+            if (checkPayment())
+            {
+                await publishEndpoint.Publish(new PaymentCompleted { OrderId = context.Message.OrderId });
+            }
+            else
+            {
+                await publishEndpoint.Publish(new PaymentFailed { OrderId = context.Message.OrderId, Message = "CCV Hatalı" });
+            }
+        }
+
+        private bool checkPayment()
+        {
+            return new Random().Next(1, 10) % 2 == 0;
+        }
+    }
+}
